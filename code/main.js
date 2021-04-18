a7264e6fe4576f4f13c92836d5f0c80123bd4c75
Gitthuma <george.githuma@gmail.com>
2021-04-18 22:16:45 +0300

    Created three variables that hold references to the list (<ul>), <input>, and <button> elements.

---
+++ code/main.js
@@ -0,0 +1,5 @@
+/*Create three variables that hold references to the list (<ul>), <input>, and <button> elements.*/
+
+const ul = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
